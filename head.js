879cf7a6feb3cbfc2b51a641e7cfc090e1c49abb
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-19 22:08:29 +0000

    added file to return the first element of an array

--- head.js
+++ head.js
@@ -6,5 +6,9 @@ const assertEqual = function(num1, num2) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(data) {
+  return data[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
