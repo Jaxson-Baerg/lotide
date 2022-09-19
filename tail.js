692dd4d0b4d6cb78567dd5cec29facaff4cace5a
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-19 22:13:53 +0000

    added tail file to return tail of inputted array

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = function(num1, num2) {
+  if (num1 === num2) {
+    console.log(`🟩🟩🟩  Assertion Passed: ${num1} === ${num2}`);
+  } else {
+    console.log(`🟥🟥🟥  Assertion Failed: ${num1} !=== ${num2}`);
+  }
+};
+
+const tail = function(data) {
+  return data.slice(1);
+};
