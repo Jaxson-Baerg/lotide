bae4783abd9ce5ce655f044586014147693eb555
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-20 20:21:14 +0000

    Added a function file to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = require("./assertEqual.js");
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length != arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+*/
