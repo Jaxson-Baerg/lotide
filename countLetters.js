21a4c5d27087f6b5e065d7cc5ea712478650a48b
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-21 18:05:00 +0000

    added count letters function file to count how many times all letters repeat themselves inside a string

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = require("./assertEqual.js");
+
+const countLetters = function(str) {
+  const letters = str.split(" ").join("").split("");
+  let numOfLetters = {};
+
+  for (let i = 0; i < letters.length; i++) {
+    if (numOfLetters[letters[i]] === undefined) {
+      numOfLetters[letters[i]] = 0;
+    }
+    numOfLetters[letters[i]] += 1;
+  }
+
+  return numOfLetters;
+};
+
+const temp = countLetters("lighthouse");
+
+assertEqual(temp["h"], 2);
