fbfc502724b525a66b5653ad7dc23eaf1a8bbac8
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-21 19:51:00 +0000

    commented out tests

--- eqObjects.js
+++ eqObjects.js
@@ -64,6 +64,8 @@ const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
 */
 
+/*
 const cd = { c: "1", d: ["2", [3, 4]] };
 const dc = { d: ["2", [3, 4]], c: "1" };
 assertEqual(eqObjects(cd, dc), true); // => true
+*/
