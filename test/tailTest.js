51c1937b3e6e615abc8262b94aa819a22f355bc6
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-26 05:55:21 +0000

    implemented mocha testing for tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,8 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const tail = require("../tail.js");
 
-assertArraysEqual(tail([1, 2, 3]), [2, 3]);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
