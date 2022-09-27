7955e25cb8fadb2e4493ea05d2e29f32e466b803
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-27 14:01:57 +0000

    refactor and updated chai assert functions from deepequal to equal where the latter is all that's needed

--- findKeys.js
+++ findKeys.js
@@ -1,5 +1,5 @@
 const findKeys = function(obj, func) {
-  for (key in obj) { // Loop through each key
+  for (let key in obj) { // Loop through each key
     if (func(obj[key])) { // Apply passed function to key
       return key;
     }
--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -3,9 +3,9 @@ const _ = require("../index");
 
 describe("#assertObjectsEqual", () => {
   it("returns true for two objects that have equal keys and value but are out of order", () => {
-    assert.deepEqual(_.assertObjectsEqual({ c: "1", d: ["2", [3, 4]] }, { d: ["2", [3, 4]], c: "1" }), true);
+    assert.equal(_.assertObjectsEqual({ c: "1", d: ["2", [3, 4]] }, { d: ["2", [3, 4]], c: "1" }), true);
   });
   it("returns false for two objects with the same keys but different values", () => {
-    assert.deepEqual(_.assertObjectsEqual({ c: "1", d: ["2", [3, 4]] }, { d: ["2", [3, 1]], c: "1" }), false);
+    assert.equal(_.assertObjectsEqual({ c: "1", d: ["2", [3, 4]] }, { d: ["2", [3, 1]], c: "1" }), false);
   });
 });
